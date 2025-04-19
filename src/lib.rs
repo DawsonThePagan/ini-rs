8ac63c81a6c20569b55bedd9a12dd0b3ff9a4ca4
Bailey Dawson <dawson270500@gmail.com>
2025-04-19 20:51:52 +0100

    Added from_string, to_string and display

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ use std::fs::{OpenOptions};
 use std::io::{self, Write};
 use std::collections::BTreeMap;
 use std::env::consts::OS;
+use std::fmt;
 use std::path::Path;
 extern crate read_lines_with_blank;
-use read_lines_with_blank::read_lines_with_blank;
+use read_lines_with_blank::{read_lines_with_blank, read_lines_with_blank_from_str};
 
 /// Load INI files into a structured BTreeMap, then edit them.
 /// Can also create new INI files.
@@ -12,7 +13,7 @@ use read_lines_with_blank::read_lines_with_blank;
 /// This only works on Windows and Linux
 pub struct Ini {
     pub config_map: BTreeMap<String, BTreeMap<String, String>>,
-    config_file: String,
+    pub config_file: String,
 }
 
 const CONFIG_SECTION_START: &str = "[";
@@ -40,26 +41,19 @@ impl Ini {
             Err(_) => return Err(io::Error::new(io::ErrorKind::Other, "Failed to read file"))
         };
 
-        println!("Number of lines: {}", lines.len());
-
         for line in lines {
             if line.starts_with(CONFIG_COMMENT) {
-                println!("Comment line");
                 continue;
             }
             if line.len() == 0 {
-                println!("Blank line");
                 continue;
             }
-
-            println!("{}", line);
 
             // Section found
             if line.starts_with(CONFIG_SECTION_START) && line.contains(CONFIG_SECTION_END) {
                 let edit = line.replace(CONFIG_SECTION_START, "").replace(CONFIG_SECTION_END, "").trim().to_string();
                 ret.config_map.insert(edit.clone(), BTreeMap::new());
                 in_section = true;
-                println!("Found a section");
                 continue;
             }
             // KVP found
@@ -67,7 +61,7 @@ impl Ini {
                 if !in_section {
                     return Err(io::Error::new(io::ErrorKind::InvalidData, "Config file was invalid, KVP entry found before section."));
                 }
-                println!("Found a kvp split");
+
                 let kvp = match line.split_once(CONFIG_KVP_SPLIT) {
                     Some(x) => x,
                     None => return Err(io::Error::new(io::ErrorKind::InvalidData, "Config file was invalid, KVP entry couldn't be split.")),
@@ -86,7 +80,87 @@ impl Ini {
                 return Err(io::Error::new(io::ErrorKind::InvalidData, "Config file was invalid, line didn't hit any requirement"));
             }
         }
-        println!("Number of sections {}", ret.config_map.len());
+        Ok(ret)
+    }
+
+    /// Dump out the INI file to a string, returns blank string if no data is present
+    pub fn to_string(&self) -> Result<String, io::Error> {
+        let new_line = match OS {
+            "linux" => NEW_LINE_LINUX,
+            "windows" => NEW_LINE_WINDOWS,
+            _ => return Err(io::Error::new(io::ErrorKind::Unsupported, "Unsupported OS"))
+        };
+
+        let mut ret: String = String::new();
+
+        if self.config_map.is_empty() { return Ok(ret) }
+
+        for (section_k, section_v) in &self.config_map {
+            ret.push_str(CONFIG_SECTION_START);
+            ret.push_str(section_k);
+            ret.push_str(CONFIG_SECTION_END);
+            ret.push_str(new_line);
+
+            for (k,v) in section_v {
+                ret.push_str(k);
+                ret.push_str(CONFIG_KVP_SPLIT);
+                ret.push_str(v);
+                ret.push_str(new_line);
+            }
+        }
+
+        Ok(ret)
+    }
+
+    /// Create ini structure from a string. Does not set the config_file so save doesn't work unless set manually.
+    pub fn from_string(str: String) -> Result<Ini, io::Error> {
+        let mut in_section = false;
+        let mut ret = Ini{ config_map: BTreeMap::new(), config_file: "".to_string() };
+
+        let lines = match read_lines_with_blank_from_str(&str) {
+            Ok(x) => x,
+            Err(_) => return Err(io::Error::new(io::ErrorKind::Other, "Failed to read file"))
+        };
+
+        for line in lines {
+            if line.starts_with(CONFIG_COMMENT) {
+                continue;
+            }
+            if line.len() == 0 {
+                continue;
+            }
+
+            // Section found
+            if line.starts_with(CONFIG_SECTION_START) && line.contains(CONFIG_SECTION_END) {
+                let edit = line.replace(CONFIG_SECTION_START, "").replace(CONFIG_SECTION_END, "").trim().to_string();
+                ret.config_map.insert(edit.clone(), BTreeMap::new());
+                in_section = true;
+                continue;
+            }
+            // KVP found
+            else if line.contains(CONFIG_KVP_SPLIT) {
+                if !in_section {
+                    return Err(io::Error::new(io::ErrorKind::InvalidData, "Config file was invalid, KVP entry found before section."));
+                }
+
+                let kvp = match line.split_once(CONFIG_KVP_SPLIT) {
+                    Some(x) => x,
+                    None => return Err(io::Error::new(io::ErrorKind::InvalidData, "Config file was invalid, KVP entry couldn't be split.")),
+                };
+
+                let mut last = match ret.config_map.last_entry() {
+                    Some(x) => x,
+                    None => return Err(io::Error::new(io::ErrorKind::InvalidData, "Config file was invalid, KVP entry didn't have a section.")),
+                };
+
+                last.get_mut().insert(kvp.0.to_string(), kvp.1.to_string());
+
+                continue;
+            }
+            else {
+                return Err(io::Error::new(io::ErrorKind::InvalidData, "Config file was invalid, line didn't hit any requirement"));
+            }
+        }
         Ok(ret)
     }
 
@@ -95,30 +169,17 @@ impl Ini {
     /// Ok will contain the size in bytes of the file after writing.
     /// All comments in the INI file will be lost by doing this.
     pub fn save(&self) -> Result<usize, io::Error> {
-        let new_line = match OS {
-            "linux" => NEW_LINE_LINUX,
-            "windows" => NEW_LINE_WINDOWS,
-            _ => return Err(io::Error::new(io::ErrorKind::Unsupported, "Unsupported OS"))
-        };
-
-        let mut file = OpenOptions::new().create(true).write(true).truncate(true).open(&self.config_file)?;
-
-        for (section_k, section_v) in &self.config_map {
-            file.write_all(CONFIG_SECTION_START.as_bytes())?;
-            file.write_all(section_k.as_bytes())?;
-            file.write_all(CONFIG_SECTION_END.as_bytes())?;
-            file.write_all(new_line.as_bytes())?;
-
-            for (k,v) in section_v {
-                file.write_all(k.as_bytes())?;
-                file.write_all(CONFIG_KVP_SPLIT.as_bytes())?;
-                file.write_all(v.as_bytes())?;
-                file.write_all(new_line.as_bytes())?;
-            }
-
-            file.flush()?;
+        if self.config_file.is_empty() {
+            return Err(io::Error::new(io::ErrorKind::Other, "No config file was found"))
         }
 
+        let mut file = OpenOptions::new().create(true).write(true).truncate(true).open(&self.config_file)?;
+        let str = match self.to_string() {
+            Ok(x) => x,
+            Err(e) => return Err(e)
+        };
+
+        file.write_all(str.as_bytes())?;
         file.flush()?;
         file.sync_all()?;
         
@@ -160,4 +221,12 @@ impl Ini {
     pub fn remove_section(&mut self, section: &str) {
         self.config_map.remove(section);
     }   
+}
+
+/// Display trait. Returns the string dump of INI data
+impl fmt::Display for Ini {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let ret = self.to_string().unwrap();
+        write!(f, "{}", ret)
+    }
 }
